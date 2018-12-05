82e9254eece3954ae9c35e2a3dd4dca61dd8049e
Samuel Curry <samc735@gmail.com>
2018-12-04 23:26:58 -0500

    basic bot

---
+++ main.js
@@ -0,0 +1,35 @@
+const fs = require(`fs`);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const {prefix, botToken} = require(`./config`);
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(msg, args);
+	}
+	catch (error) {
+		console.error(error);
+		msg.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(botToken);
