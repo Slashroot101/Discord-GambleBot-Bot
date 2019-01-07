5405946fb3a8e709efe88d2750f640551fa76587
Samuel Curry <currysl@ucmail.uc.edu>
2019-01-07 11:05:32 -0500

    more code quality

--- api/commands.js
+++ api/commands.js
@@ -2,29 +2,29 @@ const request = require('request-promise');
 const config = require('../config');
 
 exports.create = (commands) => {
-    return new Promise(async (resolve) => {
-        let options = {
-            method: 'POST',
-            uri: `${config.apiUrl}/commands`,
-            body: commands,
-            json: true
-        }; 
+	return new Promise(async (resolve) => {
+		let options = {
+			method: 'POST',
+			uri: `${config.apiUrl}/commands`,
+			body: commands,
+			json: true,
+		};
 
-        let command = await request(options);
-        resolve(command.data);
-    });
+		let command = await request(options);
+		resolve(command.data);
+	});
 };
 
 exports.addToUserAudit = (commandID, userID) => {
-    return new Promise(async(resolve) => {
-        let options = {
-            method: 'POST',
-            uri: `${config.apiUrl}/commands/${commandID}/user/${userID}`,
-            body: {},
-            json: true
-        };
+	return new Promise(async (resolve) => {
+		const options = {
+			method: 'POST',
+			uri: `${config.apiUrl}/commands/${commandID}/user/${userID}`,
+			body: {},
+			json: true,
+		};
 
-        let commandAudit = await request(options);
-        resolve(commandAudit.data); 
-    });
-}
+		const commandAudit = await request(options);
+		resolve(commandAudit.data);
+	});
+};
