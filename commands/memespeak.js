c0377042c253e0392b075152d068409ed20f6d0a
Samuel Curry <samc735@gmail.com>
2019-01-10 17:51:53 -0500

    added a very useful command :D

---
+++ commands/memespeak.js
@@ -0,0 +1,29 @@
+module.exports = {
+	name: 'speak',
+	description: "yoU'lL JuSt hAVe To SEe",
+	requiresAdmin: false,
+	duration: 60,
+	hasCooldown: false,
+	usages: 1,
+	execute(client, message, args) {
+        let ret = '';
+        
+        if(args.length === 0){
+            return message.reply('give me a phrase to translate. Such as !speak blah blah blah');
+        }
+
+        for(let i = 0; i < args.length; i++){
+            for(let j = 0; j < args[i].length;j++){
+                let isCaps = Math.floor((Math.random() * 3) + 1);
+                if(isCaps === 1){
+                    ret += args[i].charAt(j).toUpperCase();
+                } else {
+                    ret += args[i].charAt(j);
+                }
+            }
+            ret += " ";
+        }
+
+        message.reply(ret)
+	},
+};
