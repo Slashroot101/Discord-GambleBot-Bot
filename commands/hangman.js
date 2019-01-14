68efcd855bebfe724d2c1c06d86d32db64fd8f01
Samuel Curry <samc735@gmail.com>
2019-01-13 23:49:25 -0500

    some hangman stuff

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const {prefix} = require(`../config`);
 
 module.exports = {
 	name: 'hangman',
@@ -7,7 +8,7 @@ module.exports = {
 	duration: 60,
 	hasCooldown: false,
 	usages: 1,
-	execute(client, message, args, user) {
+	async execute(client, message, args, user) {
         const bet = Number(parseInt(args[0]));
 
 		if(bet < 0){
@@ -22,10 +23,29 @@ module.exports = {
 			return message.reply(`you do not have enough money! You currently have ${user.current_balance}, and would need to gain $${bet - user.current_balance} more to make that bet.`);
         }
 
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 60000 });
-        
-    	collector.on('collect', msg => {
-            console.log(msg)
-        });
+		let privateMessage = await message.author.send('To start hangman I need a sentence/word from you, type `!sentence <word/sentence>` such as `!sentence wow this bot is awesome`. Please keep in mind that only letters are allowed in hangman. You have 30 seconds.');
+
+		console.log(privateMessage.channel.id)
+
+        const dmCollector = new Discord.MessageCollector(privateMessage.channel, m => m.author.id === message.author.id, { time: 30000 });
+		let sentence;
+		let currentGuessedSetence = '';
+    	dmCollector.on('collect', msg => {
+			const args = msg.content.slice(prefix.length).split(/ +/);
+			const command = args.shift().toLowerCase();
+			
+			if(command === `sentence`){
+				sentence = msg.content.substr(prefix.length + 9, msg.content.length).replace(/\s+/g, " ");
+				if(!sentence.match(/^[ A-Za-z]+$/)){
+					return message.author.send('Your sentence may only contain letters.');
+				}
+
+				currentGuessedSetence = sentence.replace(/[A-Za-z]/g, "-/");
+				message.channel.send(`A game of hangman has been started. Here is the sentence you need to guess: ${currentGuessedSetence}`);
+				return dmCollector.stop();
+			}
+		});
+		
+
 	},
 };
