50f94576094a37632ca1d3bd6443b5f29b642abc
Samuel Curry <currysl@ucmail.uc.edu>
2019-01-26 21:36:27 -0500

    setup framework for net command

--- commands/netPoints.js
+++ commands/netPoints.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 module.exports = {
 	name: 'net',
 	description: 'See how much you have netted off of each command. `Ex: !net bj`',
@@ -6,7 +9,24 @@ module.exports = {
 	hasCooldown: false,
 	usages: 1,
 	generatesMoney: false,
-	execute(client, message) {
-		message.channel.send('Pong.');
+	async execute(client, message, args, user) {
+		console.log(args)
+		if(args.length === 0){
+			message.reply('You must supply a command to check your net values on. Such as `!net bj`');
+			return;
+		}
+		let command;
+		console.log(commandFiles)
+		for (const file of commandFiles) {
+			const requiredCommand = require(`./${file}`);
+			if(requiredCommand.name === args[0]){
+				command = requiredCommand;
+				break;
+			}
+		}
+
+		console.log(command)
+
+
 	},
 };
--- main.js
+++ main.js
@@ -58,6 +58,7 @@ client.on('message', async msg => {
 		}
 	}
 	catch (error) {
+		console.log(error)
 		msg.reply('there was an error trying to execute that command!');
 	}
 });
