d97976c5ec110fec57ac77513c0571eb435b5ba8
Samuel Curry <currysl@ucmail.uc.edu>
2019-01-27 19:06:51 -0500

    #2 net command finished

--- commands/netPoints.js
+++ commands/netPoints.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const Points = require('../api/points');
 
 module.exports = {
 	name: 'net',
@@ -10,23 +11,28 @@ module.exports = {
 	usages: 1,
 	generatesMoney: false,
 	async execute(client, message, args, user) {
-		console.log(args)
-		if(args.length === 0){
+		if(args.length === 0) {
 			message.reply('You must supply a command to check your net values on. Such as `!net bj`');
 			return;
 		}
 		let command;
-		console.log(commandFiles)
 		for (const file of commandFiles) {
 			const requiredCommand = require(`./${file}`);
-			if(requiredCommand.name === args[0]){
+			if(requiredCommand.name === args[0]) {
 				command = requiredCommand;
 				break;
 			}
 		}
-
-		console.log(command)
-
-
+		if(command.generatesMoney) {
+			const netPoints = await Points.getNetCommandPoints(command.id, user.user_id);
+			console.log(netPoints.length)
+			message.reply(`you have netted $${netPoints.length ? netPoints.netPoints.total_points_gained : 0} off of ${command.name}.`);
+		}
+		else if(!command.generatesMoney) {
+			message.reply(`you have netted $0 off of ${command.name}. This command does not generate money.`);
+		}
+		else {
+			message.reply('that command does not exist.');
+		}
 	},
 };
