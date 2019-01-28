70590f7b72e7e3e4af85bc47b7f7dbcdb21bfe30
Samuel Curry <currysl@ucmail.uc.edu>
2019-01-28 00:37:55 -0500

    WIP coin flip command #15

---
+++ commands/coinFlip.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'coinflip',
+	description: 'Settle a bet with a coin flip. Ex `!coinflip heads`',
+	requiresAdmin: false,
+	duration: 60,
+	hasCooldown: false,
+	generatesMoney: true,
+	usages: 1,
+	async execute(client, message, args, user) {
+	},
+};
