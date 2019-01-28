8da705d0053090caecdea90775d962b970354255
Samuel Curry <samc735@gmail.com>
2019-01-28 13:25:24 -0500

    fixed net points command

--- commands/daily.js
+++ commands/daily.js
@@ -9,8 +9,10 @@ module.exports = {
 	generatesMoney: true,
 	usages: 1,
 	execute: async (client, message, args, user) => {
-		const reward = await daily(user.user_id);
-		message.reply(`you have been awarded $${reward} for the day!`);
-		return;
+		return new Promise(async (resolve) => {
+			const reward = await daily(user.user_id);
+			message.reply(`you have been awarded $${reward} for the day!`);
+			return resolve(reward);
+		});
 	},
 };
