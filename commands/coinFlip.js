6253e9c1d77504ba2bfe768af9a81f9df1d65aa4
Samuel Curry <currysl@ucmail.uc.edu>
2019-01-29 15:58:32 -0500

    #15 finished coinflip command

--- commands/coinFlip.js
+++ commands/coinFlip.js
@@ -1,11 +1,14 @@
 module.exports = {
 	name: 'coinflip',
-	description: 'Settle a bet with a coin flip. Ex `!coinflip heads`',
+	description: 'Settle a bet with a coin flip. Ex. `!coinflip`',
 	requiresAdmin: false,
 	duration: 60,
 	hasCooldown: false,
-	generatesMoney: true,
+	generatesMoney: false,
 	usages: 1,
 	async execute(client, message, args, user) {
+		const flip = Math.floor(Math.random() * Math.floor(2));
+		const side = flip === 1 ? 'heads' : 'tails';
+		message.reply(` the coin landed on ${side}`);
 	},
 };
