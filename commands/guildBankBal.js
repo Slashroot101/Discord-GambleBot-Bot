4869c443196e7d558466098adc53fa115ffe24ec
Samuel Curry <samc735@gmail.com>
2019-03-03 23:47:01 -0500

    new command

---
+++ commands/guildBankBal.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'gbalance',
+  description: 'Shows the balance of the guild. `!gbalance`',
+  hasCooldown: true,
+  duration: 1,
+  requiresAdmin: false,
+  generatesMoney: true,
+  usages: 1,
+  execute: async (client, message, args, user) => new Promise(async (resolve) => {
+
+
+  }),
+};
