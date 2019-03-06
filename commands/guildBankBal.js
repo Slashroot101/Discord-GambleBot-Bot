6b6e63db559cade648d650aa850f869706e1d47e
Samuel Curry <samc735@gmail.com>
2019-03-05 23:12:03 -0500

    fixed help command

--- commands/guildBankBal.js
+++ commands/guildBankBal.js
@@ -10,7 +10,26 @@ module.exports = {
   usages: 1,
   execute: async (client, msg) => {
     const guildBank = await getGuildBankByGuildID(msg.guild.id);
-    msg.channel.send(`${msg.guild.name} has \`$${guildBank.points}\` in its account.`);
+	  const embed = {
+		  color: 0x00ff00,
+		  author: {
+			  name: msg.member.user.tag,
+			  icon_url: msg.member.user.avatarURL,
+		  },
+		  title: `${msg.guild.name}`,
+		  url: '',
+		  description: '',
+		  fields: [
+			  {
+				  name: '**Current Balance**',
+				  value: `${guildBank.points}`,
+				  inline: true,
+			  },
+		  ],
+		  timestamp: new Date(),
+	  };
+
+	  msg.channel.send({ embed });
     return undefined;
   },
 };
