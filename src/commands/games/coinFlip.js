c6b50d4f3eaf39cdaa9d8ce2abb2986fb932d045
Samuel Curry <samc735@gmail.com>
2019-04-13 00:07:27 -0400

    Create coinFlip.js

---
+++ src/commands/games/coinFlip.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'coinflip',
+  description: 'Flips a coin',
+  costData: {
+    cost: 1,
+    hasCost: false,
+  },
+  cooldown: {
+    hasCooldown: true,
+    executions: 1,
+    cooldownInMinutes: 2
+  },
+  group: 'Games',
+  allowedRoles: [
+  ],
+  async execute(client, message) {
+    const flip = Math.floor(Math.random() * Math.floor(2));
+    const side = flip === 1 ? 'heads' : 'tails';
+    message.reply(` the coin landed on ${side}`);
+  },
+};
