b830541421e27049be906c43635fcc3c7aec07cc
Samuel Curry <currysl@ucmail.uc.edu>
2019-06-03 12:42:10 -0400

    some file path fixes

--- src/utility/createCommands.js
+++ src/utility/createCommands.js
@@ -2,13 +2,13 @@ const Command = require('../api/command');
 const fs = require('fs');
 
 module.exports = async (client) => {
-  const commandDirectories = fs.readdirSync('./commands');
+  const commandDirectories = fs.readdirSync(`${process.cwd()}/src/commands`);
   const allCommands = await Command.getWithFilter();
   const commands = new Map();
   for (const directory of commandDirectories) {
-    const files = fs.readdirSync(`commands/${directory}`);
+    const files = fs.readdirSync(`${process.cwd()}/src/commands/${directory}`);
     files.forEach(async (file) => {
-      const command = require(`../commands/${directory}/${file}`);
+      const command = require(`${process.cwd()}/src/commands/${directory}/${file}`);
       let foundCommand = allCommands.filter(x => x.name === command.name);
       if(!foundCommand.length){
         command.isInMaintenanceMode = false;
